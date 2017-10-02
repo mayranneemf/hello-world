f7b1462354667cf17f593f24b85828d1a7d7fac5
Mayranne Furtunato <mayranneemf@live.com>
2017-10-02 09:58:35 -0300

    Teste

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,6 @@
 int main
 cout << " Hello wolrd" << endl; 
 cout << " Mudei" << endl; 
+cout << "Me chamo Mayranne Émylle Medeiros Furtunato, sou aluna do curso de Engenharia Mecatrôcnia da UFRN e estou no trainee da Include Engenharia" << endl;
 
 return 0; 
